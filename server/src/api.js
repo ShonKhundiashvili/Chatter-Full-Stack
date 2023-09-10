71d82d7d1fad2e3228f3f28c07aba38d5b00696d
ShonKhundiashvili <shonkhundiashvili@gmail.com>
2023-09-10 09:01:41 +0300

    now I have users and chat so i added users

--- server/src/api.js
+++ server/src/api.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const chat = require("./chat/router");
+const users = require("./users/router");
 router.use("/chat", chat);
+router.use("/users", users);
 
 module.exports = router;

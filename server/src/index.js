e2281535fc030dfe3df19c8ba94d456e9e68aa90
ShonKhundiashvili <shonkhundiashvili@gmail.com>
2023-09-10 09:00:31 +0300

    socket init

--- server/src/index.js
+++ server/src/index.js
@@ -4,17 +4,16 @@ const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
 const api = require("./api");
+const socketInit = require("./sockets");
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api", api);
-// app.post("/", (req, res) => {
-//   res.send();
-// });
 
-app.listen(8080, () => {
-  console.log("The server is running on port 8080");
+const server = socketInit(app);
+
+server.listen(process.env.PORT, () => {
+  console.log(`The server is running on port ${process.env.PORT}`);
 });

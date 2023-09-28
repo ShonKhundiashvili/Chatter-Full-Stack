8a4dc38665fc4c2a2d15b82d9136708b6365630c
ShonKhundiashvili <shonkhundiashvili@gmail.com>
2023-09-28 23:00:10 +0300

    added cookie parser middleware

--- server/src/index.js
+++ server/src/index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const api = require("./api");
 const socketInit = require("./sockets");
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
